1e50585b30d0cc0ed6bf9399b8db04424fdd4ac8
Dipak Rathod <dipakrathod.work@gmail.com>
2026-08-12 00:08:40 +0530

    fix: replace Sidebar import with AppSidebar to bypass git casing bug

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Sidebar from '../components/Sidebar.jsx';  
+import Sidebar from '../components/AppSidebar';  
 import TopHeader from '../components/TopHeader';
 import SearchModal from '../components/SearchModal';
 
